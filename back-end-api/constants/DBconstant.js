3fe651cc497fe7368967d2932903a932573538cb
SaurabhDadhe <saurabhdadhe1997@gmail.com>
2024-08-26 15:29:24 +0530

    1.1

---
+++ back-end-api/constants/DBconstant.js
@@ -0,0 +1,4 @@
+export const DB_URL = "mongodb://127.0.0.1:27017/seatmanagement";
+
+// export const DB_URL = "mongodb+srv://saurabhdadhe1997:s6yFUF3i3oi5uCt7@seatmanage.jsjcqlu.mongodb.net/?retryWrites=true&w=majority&appName=seatmanage";
+
